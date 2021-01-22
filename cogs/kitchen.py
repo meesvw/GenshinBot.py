1de4eac4be453f02d56744da53da5f5d97fffc8d
meesvw <mees011@hotmail.nl>
2021-01-23 00:11:21 +0100

    Reorganized data folder. Prepared kitchen.py for future usage.

--- cogs/kitchen.py
+++ cogs/kitchen.py
@@ -1,3 +1,25 @@
 import discord
+import os
 from discord.ext import commands
 
+
+# Vars
+
+
+# Functions
+
+
+# Creates class
+class Kitchen(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    # Create Traveler profile.
+    @commands.command()
+    async def cook(self, ctx):
+        await ctx.send("Pong!")
+
+
+def setup(bot):
+    bot.add_cog(Kitchen(bot))
